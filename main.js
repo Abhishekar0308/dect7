598018e0364dc65362613211827f6d8c5d7197a1
Abhishekar0308 <88946903+Abhishekar0308@users.noreply.github.com>
2022-07-08 17:46:46 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+video = "";
+status1="";
+function setup() { 
+    canvas = createCanvas(380, 380); 
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(380, 380);
+    video.hide();
+ }
+
+function start()
+{
+     objectDetector = ml5.objectDetector('cocossd', modelLoaded); 
+document.getElementById("status").innerHTML = "Status : Detecting Objects";
+}
+function draw()
+{
+    image (video,0,0,380,380);
+}
+function modelLoaded()
+{
+    console.log("model Loaded");
+    status1 =true;
+
+}
